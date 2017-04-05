bfd6279363d8ca0ca7c54dd1138038bd9dfb31e5
Arnaud Mondon <arnaud.mondon@gmail.com>
2017-04-05 10:17:35 +0200

    Ajout fonctionnalités coloration des cases jouables

--- script/script.js
+++ script/script.js
@@ -33,15 +33,52 @@ $('#plateau').html(html);
 
 $("#plateau td:not(:empty)").mouseover(function (){
 	console.log($(this).children("img").attr("src"));
+	console.log($(this).attr("id"));
+	var idCell=$(this).attr("id");
+	var col=idCell[6]; 
+	var line=idCell[9];
+	console.log(line+" ; "+col);
 	if ($(this).children("img").attr("src")=="img/blue.png" && tourJ2==false){
-		$(this).removeClass("noir").removeClass("blanc").addClass("rouge");
+		$(this).removeClass("noir").addClass("vert");
+		if(line<8){
+			if(col>1){
+				if ($("td#cell-c"+(col-1)+"-l"+(line-(-1))+" img").length<1){
+					$("td#cell-c"+(col-1)+"-l"+(line-(-1))).removeClass("noir").addClass("vert");
+				}
+			}
+			if(col<8){
+				if ($("td#cell-c"+(col-(-1))+"-l"+(line-(-1))+" img").length<1){
+					$("td#cell-c"+(col-(-1))+"-l"+(line-(-1))).removeClass("noir").addClass("vert");
+				}
+			}
+		}
+		
 	}
 	else if ($(this).children("img").attr("src")=="img/red.png" && tourJ2==true){
-		$(this).removeClass("noir").removeClass("blanc").addClass("rouge");
+		$(this).removeClass("noir").addClass("vert");
+		if(line>1){
+			if(col<8){
+				if ($("td#cell-c"+(col-(-1))+"-l"+(line-1)+" img").length<1){
+					$("td#cell-c"+(col-(-1))+"-l"+(line-1)).removeClass("noir").addClass("vert");
+				}
+			}
+			if(col>1){
+				if ($("td#cell-c"+(col-1)+"-l"+(line-1)+" img").length<1){
+					$("td#cell-c"+(col-1)+"-l"+(line-1)).removeClass("noir").addClass("vert");
+				}
+			}
+		}
 	}
 	
 
 
 }).mouseout(function(){
-	$(this).removeClass("rouge").addClass("noir");
+	$(this).removeClass("vert").addClass("noir");
+	var idCell=$(this).attr("id");
+	var col=idCell[6]; 
+	var line=idCell[9];
+	$("td#cell-c"+(col-1)+"-l"+(line-(-1))).removeClass("vert").addClass("noir");
+	$("td#cell-c"+(col-(-1))+"-l"+(line-(-1))).removeClass("vert").addClass("noir");
+	$("td#cell-c"+(col-1)+"-l"+(line-1)).removeClass("vert").addClass("noir");
+	$("td#cell-c"+(col-(-1))+"-l"+(line-1)).removeClass("vert").addClass("noir");
 });
